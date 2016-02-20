a547b686154271febe409487a7e2ec2a356375b0
hmm007 <hmmpython@gmail.com>
2016-02-20 11:57:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.listen(process.env.PORT || 3500);
 app.get('/', function(req, res) {
 
      res.json({
-        ipaddress: req.headers['x-forwarded-for'] ||
-                   req.connection.remoteAddress,
+        ipaddress: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
         language: req.headers["accept-language"].split(",")[0],
         software: req.headers["user-agent"].match(/\((.*?)\)/)[1]
     });
